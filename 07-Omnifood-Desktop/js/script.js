15e164f6ace2fccde8e1325437239b5da95880dd
ucangun <149247682+ucangun@users.noreply.github.com>
2024-04-24 23:00:24 +0200

    add Sticky nav bar with js

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -2,10 +2,32 @@ console.log("Hello world!");
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector("header");
-
+/////// Close Mobil Navigation
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+/////// Sticky Nav
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
